34bb4df6256cf697b66136fee2f383cbeea04ffc
bungulatest <ilayluz@gmail.com>
2015-04-27 01:41:31 +0300

    Added GPS location to currentUser, and saved to datastore

--- app/src/main/java/com/mycompany/ofytest/TrempitActivity.java
+++ app/src/main/java/com/mycompany/ofytest/TrempitActivity.java
@@ -2,6 +2,8 @@ package com.mycompany.ofytest;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.media.browse.MediaBrowser;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,6 +16,9 @@ import android.widget.ListView;
 import com.example.ilay.myapplication.backend.trempitApi.TrempitApi;
 import com.example.ilay.myapplication.backend.trempitApi.model.Event;
 import com.example.ilay.myapplication.backend.trempitApi.model.TrempitUser;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
@@ -25,15 +30,27 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class TrempitActivity extends ActionBarActivity {
+public class TrempitActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
     ArrayList<Event> events = new ArrayList<>();
     EventAdapter eventAdapter;
     TrempitUser currentUser = new TrempitUser();
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient googleApiClient;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location lastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trempit);
+        buildGoogleApiClient();
+
         eventAdapter = new EventAdapter(this, events);
 
         new EndpointsAsyncTask(this).executeOnExecutor(EndpointsAsyncTask.THREAD_POOL_EXECUTOR);
@@ -78,6 +95,68 @@ public class TrempitActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (lastLocation != null) {
+            com.example.ilay.myapplication.backend.trempitApi.model.Location location = new com.example.ilay.myapplication.backend.trempitApi.model.Location();
+            location.setLatitude((float) lastLocation.getLatitude());
+            location.setLongitude((float) lastLocation.getLongitude());
+            location.setId((long) 1000);
+            currentUser.setHomeLocation(location);
+        } else {
+            Log.d("TrempIt", "could not find location");
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.d("TrempIt", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.d("TrempIt", "Connection suspended");
+        googleApiClient.connect();
+    }
+
      class EndpointsAsyncTask extends AsyncTask<Void, Void, List<Event>> {
         private  TrempitApi myApiService = null;
         private Context context;
@@ -106,6 +185,8 @@ public class TrempitActivity extends ActionBarActivity {
             }
 
             try {
+                myApiService.insertLocation(currentUser.getHomeLocation()).execute();
+                myApiService.insertTrempitUser(currentUser).execute();
                 return myApiService.listEvents().execute().getItems();
             } catch (IOException e) {
                 Log.d("Trempit", "IO error");
