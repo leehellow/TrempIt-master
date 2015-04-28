590a0ed288011ab2edde7a59ca37d672d4fbec2e
leehellow <lee.hellow1@gmail.com>
2015-04-28 19:44:49 +0300

    Fixed Request button and added toaster

--- app/src/main/java/com/mycompany/ofytest/DriversActivity.java
+++ app/src/main/java/com/mycompany/ofytest/DriversActivity.java
@@ -45,7 +45,7 @@ public class DriversActivity extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drivers);
-
+        //
         globalState = (GlobalState) getApplicationContext();
         currentUser = globalState.getCurrentUser();
 
